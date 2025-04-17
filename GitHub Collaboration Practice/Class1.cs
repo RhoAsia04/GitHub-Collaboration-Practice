2abe3b96c4fa43724186966b19d2a4ea702d8a4f
Rhoda Asiamah <asiamaro@mail.uc.edu>
2025-04-17 12:03:09 -0400

    I said hello everyone

---
+++ GitHub Collaboration Practice/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub_Collaboration_Practice
+{
+    class Class1
+    {
+        public static void HelloWorld()
+        {
+            Console.WriteLine("Hello Everyone");
+    
+        }
+
+
+
+    }
+}
